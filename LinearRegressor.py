9e2eaafa4d52e4492c2cc0ac44bbbc9a7445de53
ilyes-aa11 <ilyesoukil777@gmail.com>
2025-09-05 16:35:32 +0100

    Update LinearRegressor.py

--- LinearRegressor.py
+++ LinearRegressor.py
@@ -2,64 +2,57 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+class LinearRegressor:
+    def __init__(self,features_num):
+        self.weights = np.zeros(features_num+1)
+
+    def train(self,X_train,Y_train):
+        bias = np.ones((X_train.shape[0],1))
+        X = np.hstack((X_train,bias))
+
+        def cost(X,Y):
+            Y_pred = np.dot(X,self.weights)
+            result = np.sum((Y - Y_pred) ** 2)
+            return result
+        
+        def gradient_cost(X,Y):
+            Y_pred = np.dot(X,self.weights)
+            gradient_vector = -2 * np.dot(X.T,Y - Y_pred)
+            return gradient_vector
+        
+        def gradient_descent(X,Y,learning_rate,max_iteration,epsilon):
+            for i in range(max_iteration):
+                old_cost = cost(X,Y)
+                print(i , old_cost)
+                self.weights = self.weights - learning_rate * gradient_cost(X,Y)
+                new_cost = cost(X,Y)
+                if abs(new_cost - old_cost) < epsilon:
+                    break
+
+        gradient_descent(X,Y_train,0.001,10000,1e-5)
+
+    def predict(self,X_test):
+        bias = np.ones((X_test.shape[0],1))
+        X = np.hstack((X_test,bias))
+        return np.dot(X,self.weights)
+
+
+# test1 for 2 dimesnsions
 data = pd.read_csv("./datasets/score.csv")
 
-# x & y are two list containing the data points
-def linear_regressor(x: list,y: list):
-    m , b = 0.0 , 0.0 
-    alpha = 0.001
+X , y = data["Hours"].to_numpy() , data["Scores"].to_numpy()
+X = np.reshape(X,(-1,1))
 
-    def error(x: list,y: list,m: float,b: float):
-        err = 0
-        for i in range(len(y)):
-            err += (y[i] - m*x[i] - b)**2
-        err /= len(y)
+clf = LinearRegressor(1)
+clf.train(X,y)
 
-        return err
+x_test = np.linspace(0, 9, 20).reshape(-1, 1) 
+y_pred = clf.predict(x_test)
 
-    def partial_err_partial_m(m: float,b: float):
-        res = 0
-        for i in range(len(y)):
-            res += x[i]*(y[i] - m*x[i] - b)
-        res *= (-2/len(y))
-        
-        return res
-
-    def partial_err_partial_b(m: float,b: float):
-        res = 0
-        for i in range(len(y)):
-            res += y[i] - m*x[i] - b
-        res *= -2
-
-        return res
-    
-    old_err = error(x,y,m,b)
-    for i in range(10000):
-        print(i,old_err)
-        old_m = m
-        old_b = b
-        m = old_m - alpha*partial_err_partial_m(old_m,old_b)
-        b = old_b - alpha*partial_err_partial_b(old_m,old_b)
-        new_err = error(x,y,m,b)
-        if abs(new_err - old_err) < 0.0001:
-            break
-        else:
-            old_err = new_err
-
-    return (m,b)
-
-X , y = data["Hours"].tolist() , data["Scores"].tolist()
-
-m , b = linear_regressor(X,y)
-
-
-x_vals = np.linspace(data["Hours"].min(), data["Hours"].max(), 100)  # smooth range
-y_vals = m * x_vals + b
-
-plt.scatter(data["Hours"],data["Scores"],color="blue")
-plt.plot(x_vals,y_vals,color="red")
+plt.scatter(X, y, color="blue", label="Data Points")
+plt.plot(x_test, y_pred, color="red", label="Prediction Line")
+plt.xlabel("Hours")
+plt.ylabel("Scores")
+plt.legend()
 plt.show()
-
-
-
-
